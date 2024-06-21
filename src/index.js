c442295f335ff314893fe864770e08c69232fc76
RAJAT DUGAR <rajat02514@gmail.com>
2024-06-21 13:38:48 +0530

    auth partly done

---
+++ src/index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const { PrismaClient } = require("@prisma/client");
+
+const PORT = 3000;
+const SECRET_KEY = "webops2024"; //for signing the jwt token
+const app = express();
+const prisma = new PrismaClient();
+console.log("Connected to the DB");
+
+//define the interfaces
+
+//add the necessary middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+//index route
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+//auth routes
+app.post("/signup", (req, res) => {
+  const { phone, password, email } = req.body;
+  const saltRounds = 10;
+
+  console.log(phone, password, email);
+
+  //hashing the password using bcrypt js
+
+  bcrypt
+    .hash(password, saltRounds)
+    .then(async (hashedPassword) => {
+      //create the user here.
+      console.log("Password hashed successfully.");
+      const user = await prisma.User.create({
+        data: {
+          phone: phone,
+          password: hashedPassword,
+          email: email,
+        },
+      });
+      if (user) {
+        res.status(200).json({ message: "User created successfully." });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Server error: " + err }); //this here would basically return an error if the username is already taken. or for any other error, the error will be sent to the frontend.
+    });
+});
+
+app.post("/signin", async (req, res) => {
+  const { phone, password } = req.body;
+
+  //check if the user exists on the DB,if exists retreive it and compare the password
+  const user = await prisma.user.findUnique({
+    where: {
+      phone: phone,
+    },
+  });
+  if (user) {
+    //if user exists, log him in
+    const hashedPassword = user.password;
+    bcrypt
+      .compare(password, hashedPassword) //hashedPassword will be retreived from the database.
+      .then((result) => {
+        if (result) {
+          //now send the token to the client and make it store in the localstorage.
+          const token = jwt.sign({ userId: user.id }, SECRET_KEY);
+          res.status(200).json({ token: token, message: "Login successful" });
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({ message: "Internal server error" });
+      });
+  } else {
+    res.status(401).json({ message: "No user found with the given username." });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
